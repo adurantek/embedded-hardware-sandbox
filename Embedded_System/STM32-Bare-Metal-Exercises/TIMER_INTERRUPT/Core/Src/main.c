d761f56396d5796adaddefb89fb1c1fe30c881a9
Ahmet <adurantek@gmail.com>
2026-07-19 02:04:00 +0300

    Complete the project
    Completed timer interrupt implementation

--- Embedded_System/STM32-Bare-Metal-Exercises/TIMER_INTERRUPT/Core/Src/main.c
+++ Embedded_System/STM32-Bare-Metal-Exercises/TIMER_INTERRUPT/Core/Src/main.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 #include "stm32c0xx.h"
 
-uint8_t trigger_n;
+volatile uint8_t trigger_n;
 
-void setup(void) {
+void setup (void) {
+
+	RCC -> IOPENR |= (1 << 0); //PA OPEN
+	RCC -> APBENR1 |= (1 << 1); //TIM3 OPEN
+
+
+	GPIOA -> MODER &= ~(1 << 1); //PA0 OUTPUT - REDLED
+
+	//TIMER
+
+	TIM3 -> CNT = 0; //TIME RESET
+	TIM3 -> PSC = 11999; //CONFİG FREQUENCY
+	TIM3 -> ARR = 1000; //TARGET TIME
+	TIM3 -> CR1 |= (1 << 0);
 
 	//TIMER-INTERRUPT
 
-	TIM3 -> DMA |= (1 << 0); //TIMER INTERRUPT ENABLE
+	TIM3 -> DIER |= (1 << 0); //TIMER INTERRUPT ENABLE
 	NVIC_EnableIRQ(TIM3_IRQn); //NVI TIMER INTERRUPT ENABLE
 
 }
 
 void TIM3_IRQHandler(void) {
-	trigger_n = 1;
-	TIM3 -> SR &= ~(1 << 0);
+
+	if (TIM3 -> SR & (1 << 0)) {
+
+		trigger_n = 1;
+
+	}
+
+	TIM3 -> SR &= ~(1 << 0); //RESET FLAG
 }
 
-void main (void) {
+int main (void) {
+
+	setup();
 
 	while(1) {
+		if (trigger_n == 1) {
 
+				GPIOA -> ODR ^= (1 << 0); //TOGGLE
+				trigger_n = 0;
+			}
 	}
 }
